4edffcb59abeb12b4e30a9b7442966a5481b9773
Ali-Dmerdash <alidmerdash@gmail.com>
2023-11-10 20:46:48 +0200

    added dependancies to connect to databse

--- Project/src/main/java/module-info.java
+++ Project/src/main/java/module-info.java
@@ -3,6 +3,13 @@ module com.example {
     requires javafx.fxml;
     requires transitive javafx.graphics;
     
+    requires org.mongodb.driver.sync.client;
+    requires transitive org.mongodb.bson;
+    requires org.mongodb.driver.core;
+
+    requires org.slf4j;
+    requires ch.qos.logback.classic;
+
     opens com.example to javafx.fxml;
     exports com.example;
 }
