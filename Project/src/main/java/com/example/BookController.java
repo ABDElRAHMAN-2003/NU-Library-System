fdab40a6a3f2c581282c8b4a418cfb3d560164cc
Abdelrahman Magdi <A.Magdi2190@nu.edu.eg>
2023-12-02 23:07:44 +0200

    displaying books done

--- Project/src/main/java/com/example/BookController.java
+++ Project/src/main/java/com/example/BookController.java
@@ -1,47 +1,45 @@
 package com.example;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 public class BookController {
 
     @FXML
-    private VBox mainVBox;
-
-    @FXML
-    private ScrollPane bookDisplayScrollPane;
+    private TextField searchField;
 
     @FXML
     private VBox scrollVBox;
 
     @FXML
-    private TextField searchField;
+    private GridPane innerGridPane;
 
     public void initialize() {
-        // Example data - Display VBox elements in ScrollPane
-        displayVBoxesInScrollPane();
-    }
+        // Sample data, replace this with your actual data
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                // Create a new VBox for each iteration
+                VBox vbox = createBookVBox(i, j);
 
-    private void displayVBoxesInScrollPane() {
-        int numberOfVBoxes = 10; // Set the number of VBox elements
-
-        for (int i = 0; i < numberOfVBoxes; i++) {
-            VBox vBox = createVBoxWithIndex(i);
-            scrollVBox.getChildren().add(vBox);
+                // Add the VBox to the inner GridPane with specified row and column indices
+                innerGridPane.add(vbox, j, i);
+            }
         }
     }
 
-    private VBox createVBoxWithIndex(int index) {
-        VBox vBox = new VBox();
-        vBox.getStyleClass().add("your-vbox-style"); 
+    private VBox createBookVBox(int row, int col) {
+        // Create your VBox with the desired content
+        VBox vbox = new VBox();
+        // Add content to the VBox
+        vbox.getStyleClass().add("books");
+        // Example: Adding a Label with row and col information
+        vbox.getChildren().add(new javafx.scene.control.Label("Row: " + row + ", Col: " + col));
 
-        Label labelIndex = new Label("Index: " + index);
-        Label labelText = new Label("VBox " + index);
+        // You can customize the VBox contents based on your requirements
 
-        vBox.getChildren().addAll(labelIndex, labelText);
-        return vBox;
+        return vbox;
     }
 }
