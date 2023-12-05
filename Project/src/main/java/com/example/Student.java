abaedf7aca99cad518aff19aee9e467f93525e2e
Abdelrahman Magdi <A.Magdi2190@nu.edu.eg>
2023-12-05 11:19:08 +0200

    added user class and mongo connector (mongo will contain all database functions)

---
+++ Project/src/main/java/com/example/Student.java
@@ -0,0 +1,80 @@
+package com.example;
+
+public class Student {
+    private String id;
+    private String studentname;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+    public Student(String id, String studentname, String password, String firstName, String lastName, String phoneNumber, String email) {
+        this.id = id;
+        this.studentname = studentname;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Getters and setters for all attributes
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getstudentname() {
+        return studentname;
+    }
+
+    public void setstudentname(String studentname) {
+        this.studentname = studentname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
